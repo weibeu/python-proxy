d5a3a9e075c1dd8ddd015818dfef28cbbbde2ac2
weibeu <deepakrajko14@gmail.com>
2022-04-28 00:48:02 +0530

    Add simple startup script

---
+++ startup.sh
@@ -0,0 +1,1 @@
+rm -rf python-proxy && git clone https://github.com/weibeu/python-proxy && cd python-proxy && pip install -r requirements.txt && export PATH=$PATH:/home/container/.local/bin && python main.py
